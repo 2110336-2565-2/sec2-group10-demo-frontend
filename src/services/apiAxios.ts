ab86d72ddf9887e9fe00c1783aa266e19a91f255
NonKhuna <khunanon.rtgs@gmail.com>
2023-02-14 15:22:05 +0700

    feat: Initial Axios class

--- src/services/apiAxios.ts
+++ src/services/apiAxios.ts
@@ -1,15 +1,66 @@
-import axios from 'axios'
+import { API_HOST } from '@/configs'
+import axios, { AxiosInstance, AxiosRequestConfig } from 'axios'
+import { getSession } from 'next-auth/react'
 
-export const apiAxios = axios.create({
-  baseURL: process.env.API_URL || 'http://localhost:8000',
-  headers: {
-    Accept: 'application/json',
-    'Content-Type': 'application/json',
-  },
-})
+class AxiosClient {
+  axiosInstance: AxiosInstance
 
-// TO DO: Add token
-// apiAxios.interceptors.request.use(function (config){
-//     config.header.
+  constructor() {
+    this.axiosInstance = axios.create({
+      baseURL: API_HOST,
+    })
 
-// });
+    this.axiosInstance.interceptors.request.use(async (req) => {
+      const session = await getSession()
+      if (session?.accessToken) {
+        req.headers['Authorization'] = `Bearer ${session?.accessToken}`
+      }
+      return req
+    })
+
+    this.axiosInstance.interceptors.response.use(
+      (response) => {
+        return response
+      },
+      (error) => {
+        // TODO: add error handle
+        console.log(error.response)
+        return Promise.reject(error)
+      }
+    )
+  }
+
+  async get<T>(url: string, config?: AxiosRequestConfig) {
+    return (await this.axiosInstance.get<T>(url, config)).data
+  }
+
+  async post<T, D = unknown>(
+    url: string,
+    data?: D,
+    config?: AxiosRequestConfig
+  ) {
+    return (await this.axiosInstance.post<T>(url, data, config)).data
+  }
+
+  async put<T, D = unknown>(
+    url: string,
+    data?: D,
+    config?: AxiosRequestConfig
+  ) {
+    return (await this.axiosInstance.put<T>(url, data, config)).data
+  }
+
+  async patch<T, D = unknown>(
+    url: string,
+    data?: D,
+    config?: AxiosRequestConfig
+  ) {
+    return (await this.axiosInstance.patch<T>(url, data, config)).data
+  }
+
+  async del(url: string, config?: AxiosRequestConfig) {
+    await this.axiosInstance.delete(url, config)
+  }
+}
+
+export const http = new AxiosClient()
