53fa42db8470656b8ac9cf2e7d80b0b3ee617c1c
NonKhuna <khunanon.rtgs@gmail.com>
2023-02-15 02:18:21 +0700

    feat: background gradient color

--- src/layout/DesktopLoayput.tsx
+++ src/layout/DesktopLoayput.tsx
@@ -4,9 +4,19 @@ import { PropsWithChildren } from 'react'
 
 const DesktopLayout = ({ children }: PropsWithChildren) => {
   return (
-    <Box display="flex" height="100vh">
+    <Box display="flex" minHeight="100vh">
       <Navbar />
-      <Box sx={{ flexGrow: 1, px: 2, py: 3 }}>{children}</Box>
+      <Box
+        sx={{
+          flexGrow: 1,
+          px: 2,
+          py: 3,
+          background:
+            'linear-gradient(180deg, rgba(0,97,166,1) 0%, 8%, rgba(4,4,47,1) 68%)',
+        }}
+      >
+        {children}
+      </Box>
     </Box>
   )
 }
