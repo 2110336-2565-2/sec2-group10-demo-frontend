3c138fafc0494fa1bc4fc7e41fd8afbd26a204e8
Thrapoom Sakulpiyawong <thrapoomsa@gmail.com>
2023-02-24 15:13:13 +0700

    fix:add borderRadius to image

--- src/components/Playlist.tsx
+++ src/components/Playlist.tsx
@@ -18,9 +18,21 @@ const Playlist = (props: Props) => {
         borderRadius: '4px',
         bgcolor: 'container.light',
       }}
+      onClick={() =>
+        //Todo: change to clicked playlist page
+        console.log('click')
+      }
     >
       <Stack spacing={1} justifyContent={'center'}>
-        <Image src={props.image} width={174} height={190} alt="mock image" />
+        <Image
+          src={props.image}
+          style={{
+            borderRadius: '4px',
+          }}
+          width={174}
+          height={190}
+          alt="mock image"
+        />
         <Stack spacing={0.5}>
           <Typography variant="subtitle1">{props.playlistName}</Typography>
           <Typography variant="subtitle2" color="text.secondary">
