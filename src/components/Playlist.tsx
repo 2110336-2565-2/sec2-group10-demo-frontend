4124dd9d4e5985718d240f45cfdbdb1bdb9a7883
Thrapoom Sakulpiyawong <thrapoomsa@gmail.com>
2023-02-24 14:33:43 +0700

    fix:add shadow and change subtitile2 color

--- src/components/Playlist.tsx
+++ src/components/Playlist.tsx
@@ -13,16 +13,19 @@ const Playlist = (props: Props) => {
       py={1}
       width={190}
       height={266}
+      boxShadow="0px 2px 12px rgba(0, 0, 0, 0.5)"
       sx={{
         borderRadius: '4px',
-        bgcolor: 'container.main',
+        bgcolor: 'container.light',
       }}
     >
       <Stack spacing={1} justifyContent={'center'}>
         <Image src={props.image} width={174} height={190} alt="mock image" />
         <Stack spacing={0.5}>
           <Typography variant="subtitle1">{props.playlistName}</Typography>
-          <Typography variant="subtitle2">{props.creatorName}</Typography>
+          <Typography variant="subtitle2" color="text.secondary">
+            {props.creatorName}
+          </Typography>
         </Stack>
       </Stack>
     </Box>
