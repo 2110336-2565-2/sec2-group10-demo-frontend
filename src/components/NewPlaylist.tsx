4fd5ca1854260bd61d4b6c2568a89078b79822d5
khunanon <khunanon.rtgs@gmail.com>
2023-02-25 22:23:03 +0700

    DEMO 67 - newplaylist component (#24)
    * feat: construct newplaylist component
    
    * update: change background color
    
    * refactor: remove musicstyle
    
    * feat: add bgcolor

---
+++ src/components/NewPlaylist.tsx
@@ -0,0 +1,37 @@
+import Image from 'next/image'
+import { Box, Stack, Typography } from '@mui/material'
+import AddPlaylistLogo from '@/assets/addplaylist-icon.svg'
+
+const NewPlaylist = () => {
+  return (
+    <Box
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      columnGap={1.5}
+      bgcolor="contianer.light"
+      width="190px"
+      height="266px"
+      borderRadius="4px"
+      boxShadow="0px 2px 12px rgba(0, 0, 0, 0.5)"
+      onClick={() => {
+        // TODO: initial playlist untitled
+        console.log('click')
+      }}
+    >
+      <Stack spacing={3.5} alignContent="center">
+        <Image
+          src={AddPlaylistLogo}
+          alt="addplaylist-icon"
+          width={120}
+          height={120}
+        />
+        <Typography variant="caption" sx={{ color: 'text.primary' }}>
+          Add a new playlist
+        </Typography>
+      </Stack>
+    </Box>
+  )
+}
+
+export default NewPlaylist
