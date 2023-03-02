62e073b8357285a69664cdeb106d4664e15ca2f0
sern <sern2001@hotmail.com>
2023-03-02 23:32:38 +0700

    feat: add artist form

--- src/pages/testartist.tsx
+++ src/pages/testartist.tsx
@@ -1,16 +1,6 @@
 import ArtistForm from '@/components/ArtistForm'
-import { Stack } from '@mui/material'
 
 const testartist = () => {
-  return (
-    <Stack
-      justifyContent="center"
-      alignItems="center"
-      width="100%"
-      height="100%"
-    >
-      <ArtistForm />
-    </Stack>
-  )
+  return <ArtistForm></ArtistForm>
 }
 export default testartist
