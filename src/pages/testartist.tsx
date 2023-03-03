92561fed3e6e2ee2875b20c02f07d66da5907818
sern <sern2001@hotmail.com>
2023-03-04 00:11:57 +0700

    update: set alignment

--- src/pages/testartist.tsx
+++ src/pages/testartist.tsx
@@ -1,6 +1,16 @@
 import ArtistForm from '@/components/ArtistForm'
+import { Stack } from '@mui/material'
 
 const testartist = () => {
-  return <ArtistForm></ArtistForm>
+  return (
+    <Stack
+      justifyContent="center"
+      alignItems="center"
+      width="100%"
+      height="100%"
+    >
+      <ArtistForm />
+    </Stack>
+  )
 }
 export default testartist
