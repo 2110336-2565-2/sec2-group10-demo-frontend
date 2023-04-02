929694c62a14008a8966380889e50565bde15db5
Pondpdpr <pBoonnud@gmail.com>
2023-04-02 18:12:22 +0700

    feat: add artist row to search page

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -1,3 +1,4 @@
+import ArtistCard from '@/components/ArtistCard'
 import SingleMusicBox from '@/components/MusicIcon/SingleMusicBox'
 import PlaylistCard from '@/components/PlaylistCard'
 import SearchBar from '@/components/SearchBar'
@@ -34,7 +35,7 @@ const Search = () => {
     <Stack spacing={3.5} sx={{ margin: '84px 82px' }}>
       <SearchBar control={control} name="search" />
       <Stack spacing={1.5}>
-        <Typography variant="h2"> Music</Typography>
+        <Typography variant="h2">Music</Typography>
         <Grid container spacing={3}>
           {musics.data?.map((music) => (
             <Grid
@@ -47,7 +48,7 @@ const Search = () => {
         </Grid>
       </Stack>
       <Stack spacing={1.5}>
-        <Typography variant="h2"> Playlists</Typography>
+        <Typography variant="h2">Playlists</Typography>
         <Grid container spacing={3}>
           {playlists.data?.map((playlist) => (
             <Grid
@@ -59,6 +60,20 @@ const Search = () => {
                 playlistName={playlist.name}
                 creatorName={playlist.creatorName}
                 image={playlist.coverImage}
+              />
+            </Grid>
+          ))}
+        </Grid>
+      </Stack>
+      <Stack spacing={1.5}>
+        <Typography variant="h2">Artists</Typography>
+        <Grid container spacing={3}>
+          {artists.data?.map((artist) => (
+            <Grid key={artist._id} {...{ xs: 2.4, sm: 2.4, md: 2.4, lg: 2.4 }}>
+              <ArtistCard
+                id={artist._id}
+                artistName={artist.username}
+                image={artist.coverImage}
               />
             </Grid>
           ))}
