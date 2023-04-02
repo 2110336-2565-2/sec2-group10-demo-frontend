6d8c4966f1ee1b067aad99afc5a122f48803b0d6
Pondpdpr <pBoonnud@gmail.com>
2023-04-02 18:46:07 +0700

    feat: component disappear when there is no item in array

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -27,58 +27,71 @@ const Search = () => {
   const playlists = usePlaylistSearch(searchString)
   const artists = useArtistSearch(searchString)
 
+  const hasMusics = !!musics.data?.length
+  const hasPlaylists = !!playlists.data?.length
+  const hasArtists = !!artists.data?.length
+
   console.log('musics', musics.data)
   console.log('playlists', playlists.data)
   console.log('artists', artists.data)
 
   return (
-    <Stack spacing={3.5} sx={{ margin: '84px 82px' }}>
+    <Stack spacing={3.5} sx={{ margin: '40px 5%' }}>
       <SearchBar control={control} name="search" />
-      <Stack spacing={1.5}>
-        <Typography variant="h2">Music</Typography>
-        <Grid container spacing={3}>
-          {musics.data?.map((music) => (
-            <Grid
-              key={music.musicId}
-              {...{ xs: 2.4, sm: 2.4, md: 2.4, lg: 2.4 }}
-            >
-              <SingleMusicBox music={music} />
-            </Grid>
-          ))}
-        </Grid>
-      </Stack>
-      <Stack spacing={1.5}>
-        <Typography variant="h2">Playlists</Typography>
-        <Grid container spacing={3}>
-          {playlists.data?.map((playlist) => (
-            <Grid
-              key={playlist._id}
-              {...{ xs: 2.4, sm: 2.4, md: 2.4, lg: 2.4 }}
-            >
-              <PlaylistCard
-                id={playlist._id}
-                playlistName={playlist.name}
-                creatorName={playlist.creatorName}
-                image={playlist.coverImage}
-              />
-            </Grid>
-          ))}
-        </Grid>
-      </Stack>
-      <Stack spacing={1.5}>
-        <Typography variant="h2">Artists</Typography>
-        <Grid container spacing={3}>
-          {artists.data?.map((artist) => (
-            <Grid key={artist._id} {...{ xs: 2.4, sm: 2.4, md: 2.4, lg: 2.4 }}>
-              <ArtistCard
-                id={artist._id}
-                artistName={artist.username}
-                image={artist.coverImage}
-              />
-            </Grid>
-          ))}
-        </Grid>
-      </Stack>
+      {hasMusics && (
+        <Stack spacing={1.5}>
+          <Typography variant="h2">Music</Typography>
+          <Grid container spacing={3}>
+            {musics.data?.map((music) => (
+              <Grid
+                key={music.musicId}
+                {...{ xs: 2.4, sm: 2.4, md: 2.4, lg: 2.4 }}
+              >
+                <SingleMusicBox music={music} />
+              </Grid>
+            ))}
+          </Grid>
+        </Stack>
+      )}
+      {hasPlaylists && (
+        <Stack spacing={1.5}>
+          <Typography variant="h2">Playlists</Typography>
+          <Grid container spacing={3}>
+            {playlists.data?.map((playlist) => (
+              <Grid
+                key={playlist._id}
+                {...{ xs: 2.4, sm: 2.4, md: 2.4, lg: 2.4 }}
+              >
+                <PlaylistCard
+                  id={playlist._id}
+                  playlistName={playlist.name}
+                  creatorName={playlist.creatorName}
+                  image={playlist.coverImage}
+                />
+              </Grid>
+            ))}
+          </Grid>
+        </Stack>
+      )}
+      {hasArtists && (
+        <Stack spacing={1.5}>
+          <Typography variant="h2">Artists</Typography>
+          <Grid container spacing={3}>
+            {artists.data?.map((artist) => (
+              <Grid
+                key={artist._id}
+                {...{ xs: 2.4, sm: 2.4, md: 2.4, lg: 2.4 }}
+              >
+                <ArtistCard
+                  id={artist._id}
+                  artistName={artist.username}
+                  image={artist.coverImage}
+                />
+              </Grid>
+            ))}
+          </Grid>
+        </Stack>
+      )}
     </Stack>
   )
 }
