baf5de1db34b53360e9ff031a3c6dc3f91ea72ab
Pondpdpr <pBoonnud@gmail.com>
2023-04-02 17:52:39 +0700

    fix: change playlistCard to the same size as musicCard

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -1,10 +1,13 @@
+import SingleMusicBox from '@/components/MusicIcon/SingleMusicBox'
+import PlaylistCard from '@/components/PlaylistCard'
 import SearchBar from '@/components/SearchBar'
 import {
   useArtistSearch,
   useMusicSearch,
   usePlaylistSearch,
 } from '@/queries/useSearch'
-import { Stack } from '@mui/material'
+import { Stack, Typography } from '@mui/material'
+import Grid from '@mui/material/Unstable_Grid2'
 import { useForm } from 'react-hook-form'
 
 interface SearchFieldValues {
@@ -28,8 +31,39 @@ const Search = () => {
   console.log('artists', artists.data)
 
   return (
-    <Stack>
+    <Stack spacing={3.5} sx={{ margin: '84px 82px' }}>
       <SearchBar control={control} name="search" />
+      <Stack spacing={1.5}>
+        <Typography variant="h2"> Music</Typography>
+        <Grid container spacing={3}>
+          {musics.data?.map((music) => (
+            <Grid
+              key={music.musicId}
+              {...{ xs: 2.4, sm: 2.4, md: 2.4, lg: 2.4 }}
+            >
+              <SingleMusicBox music={music} />
+            </Grid>
+          ))}
+        </Grid>
+      </Stack>
+      <Stack spacing={1.5}>
+        <Typography variant="h2"> Playlists</Typography>
+        <Grid container spacing={3}>
+          {playlists.data?.map((playlist) => (
+            <Grid
+              key={playlist._id}
+              {...{ xs: 2.4, sm: 2.4, md: 2.4, lg: 2.4 }}
+            >
+              <PlaylistCard
+                id={playlist._id}
+                playlistName={playlist.name}
+                creatorName={playlist.creatorName}
+                image={playlist.coverImage}
+              />
+            </Grid>
+          ))}
+        </Grid>
+      </Stack>
     </Stack>
   )
 }
